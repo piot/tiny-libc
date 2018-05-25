06d1fe95b108fb88b85c3f3dc932af82c9308e53
Peter Bjorklund <piot@hotmail.com>
2018-05-25 13:50:56 +0200

    More renames

--- src/lib/tiny_libc.c
+++ src/lib/tiny_libc.c
@@ -23,7 +23,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 */
-#include <tiny_libc/tiny_libc.h>
+#include <tiny-libc/tiny_libc.h>
 
 // #define TC_MEM_LEAK_FINDER
 
